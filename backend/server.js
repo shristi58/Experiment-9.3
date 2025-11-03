f2bc5dff4fab5a6d5f59e639c70da6de478db887
shristi58 <kumarishristi22004@gmail.com>
2025-11-03 21:07:56 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Backend is running on AWS EC2 instance")
+})
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the backend via Load Balancer!" })
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
